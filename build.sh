9cf18557ed5fec63cf1d503e6ee99efce3e6cfcd
Greg Frazier <glfrazier@alum.mit.edu>
2021-12-11 03:58:21 -0500

    Refactore build.sh, so that all of my projects have the same build.sh. build.vars sets the variables specific to each project.

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
-#!/usr/bin/env bash 
-set -x -e
+#!/usr/bin/env bash
+set -e
+
 KERNEL=`uname -s`
-ID=ObjectPool
-VER=0.0.1
+
+DIR=`dirname $0`
+cd $DIR
+
+. ./build.vars
 
 if [[  $KERNEL == "Linux" ]]; then
   mvn package -DskipTests
   REPO=/home/pi/mvn-repo
+  M2=~/.m2
 else
   mvn -f pom.cyg.xml package -DskipTests
   REPO=../mvn-repo
+  M2=/cygdrive/c/Users/glfra/.m2
 fi
 
 mvn install:install-file -Dfile=target/${ID}-${VER}.jar -DgroupId=com.github.glfrazier -DartifactId=${ID} -Dversion=${VER} -Dpackaging=jar -DlocalRepositoryPath=${REPO}
+rm -rf ${M2}/repository/com/github/glfrazier/${ID}
 
